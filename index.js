96e45e4d04ad8f1efff33d4713f4dd7fb8114cd7
Sammy Wu <swucode@gmail.com>
2018-02-02 06:58:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 class Driver {
-  constructor(name, startDate) {
+  constructor(name, startDate){
     this.name = name;
     this.startDate = new Date(startDate);
+  }
+
+  yearsExperienceFromBeginningOf(year) {
+    let endDate = new Date(year, 0, 1);
+    let totalYears = (endDate - this.startDate)/(1000*60*60*24*365);
+    return parseInt(totalYears);
+  }
+
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    let beginningBlock = eastWest.indexOf(this.beginningLocation.horizontal);
+    let endingBlock = eastWest.indexOf(this.endingLocation.horizontal);
+    let verticalDistance = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+    let horizontalDistance = (Math.abs(beginningBlock - endingBlock));
+    return verticalDistance + horizontalDistance
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled()/2;
+    } else {
+      return this.blocksTravelled()/3;
+    }
   }
 }
