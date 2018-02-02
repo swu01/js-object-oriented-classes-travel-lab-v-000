84c26a17c89683eecc5d0dc7d2cbd25f1605983b
Sammy Wu <swucode@gmail.com>
2018-02-02 07:00:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 class Driver {
-  constructor(name, startDate){
-    this.name = name;
-    this.startDate = new Date(startDate);
-  }
+    constructor(name, startDate) {
+        this.name = name;
+        this.startDate = new Date(startDate);
+    }
 
-  yearsExperienceFromBeginningOf(endDate) {
-      return endDate - this.startDate.getFullYear() - 1;
-  }
-}
-
+    yearsExperienceFromBeginningOf(endDate) {
+        return endDate - this.startDate.getFullYear() - 1;
+    }
 }
 
 class Route {
-  constructor(beginningLocation, endingLocation){
-    this.beginningLocation = beginningLocation;
-    this.endingLocation = endingLocation;
-  }
-
-  blocksTravelled() {
-    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
-    let beginningBlock = eastWest.indexOf(this.beginningLocation.horizontal);
-    let endingBlock = eastWest.indexOf(this.endingLocation.horizontal);
-    let verticalDistance = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
-    let horizontalDistance = (Math.abs(beginningBlock - endingBlock));
-    return verticalDistance + horizontalDistance
-  }
-
-  estimatedTime(peak) {
-    if (peak) {
-      return this.blocksTravelled()/2;
-    } else {
-      return this.blocksTravelled()/3;
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation;
+        this.endingLocation = endingLocation;
     }
-  }
+
+    blocksTravelled() {
+        let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+        const ns = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+        const ew = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+        return ns + ew;
+    }
+
+    estimatedTime(peak) {
+        if (peak === true) {
+            return (this.blocksTravelled() / 2)
+        } else {
+            return (this.blocksTravelled() / 3);
+        }
+    }
 }
