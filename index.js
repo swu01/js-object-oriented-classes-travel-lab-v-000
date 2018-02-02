369ab29f952ef8d724937c02db94e4537f0b1fe5
Sammy Wu <swucode@gmail.com>
2018-02-02 07:04:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+class Driver {
+    constructor(name, startDate) {
+        this.name = name;
+        this.startDate = new Date(startDate);
+    }
+    yearsExperienceFromBeginningOf(year) {
+        return year - this.startDate.getFullYear();
+    }
+}
 
+class Route {
+    constructor(beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation;
+        this.endingLocation = endingLocation;
+    }
+    blocksTravelled() {
+        let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+        const ns = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+        const ew = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+        return ns + ew;
+    }
+    estimatedTime(peak) {
+        if (peak === true) {
+            return (this.blocksTravelled() / 2)
+        } else {
+            return (this.blocksTravelled() / 3);
+        }
+    }
+}
